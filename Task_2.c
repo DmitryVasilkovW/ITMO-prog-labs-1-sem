5f0b3ba28a30dd00a3eef73372b17c4ea44e2409
DmitryVasilkovW <113594620+DmitryVasilkovW@users.noreply.github.com>
2023-04-27 21:58:02 +0300

    Update Task_2.c

--- Task_2.c
+++ Task_2.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-#include <cmath>
-
+#include <math.h>
 int main() 
 {
 	float first_arg, second_arg, z1, z2;
@@ -8,6 +7,6 @@ int main()
 	scanf("%f", &second_arg);
 	z1 = (sin(2 * first_arg) + sin(5 * first_arg) - sin(3 * first_arg)) / (cos(first_arg) - cos(3 * first_arg) + cos(5 * first_arg));
 	z2 = tan(3 * second_arg);
-	printf("8f\n %f\n", z1, z2);
+	printf("%f\n %f\n", z1, z1);
 	return 0;
 }
