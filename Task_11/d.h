09e790711f3e03be906aef3992f4847856b8fd87
DmitryVasilkovW <113594620+DmitryVasilkovW@users.noreply.github.com>
2023-04-27 23:29:55 +0300

    Create d.h

---
+++ Task_11/d.h
@@ -0,0 +1,12 @@
+#ifndef UNTITLED3_D_H
+#define UNTITLED3_D_H
+struct Rhombus
+		{
+				double x1, y1;
+				double x2, y2;
+				double x3, y3;
+				double x4, y4;
+		} ;
+double Perimeter (struct Rhombus);
+double Square (struct Rhombus);
+#endif //UNTITLED3_D_H
